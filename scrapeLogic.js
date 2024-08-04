6c00341713e76e71ed848c9ec7452828e63f71b8
Jagdamba Tripathi <jagdtri2003@gmail.com>
2024-08-04 14:23:35 +0530

    updated

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,4 +1,4 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer-extra');
 const userAgents = require('user-agents');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
